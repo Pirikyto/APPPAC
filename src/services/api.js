f84e7b16835d0a6f29b5d3ea903798729c827e70
Fabio <fabiopirikyto@gmail.com>
2022-06-02 14:59:03 -0300

    confirmar

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 export { posts, gets };
 
-// default options config: { expires: '1d', path: '/', domain: '', secure: '', sameSite: 'Lax' }
-
 const api = axios.create({
   timeout: 10000,
   params: { outputType: "json" },
 });
+//api.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+
 //Definição dos metodos para utilização da APIs em sistema Sankhya!!
 //Metodo Post
 const posts = async (req, res) => {
@@ -15,10 +15,8 @@ const posts = async (req, res) => {
     //Para Utilização do metodo Post a Requisição sempre feita em URL/api devido ao proxy configurado no quasar.config(nunca utilizar na apliacação)
     .post("/api", req)
     .then((response) => {
-      //console.log(response.data);
+      console.log(response.data);
       res = response.data;
-      //const hasIt = $q.cookies.has("cookies");
-      console.log($q);
     })
     .catch((error) => {
       // handle error
@@ -35,15 +33,15 @@ const posts = async (req, res) => {
 const gets = async (req, res) => {
   api.defaults.params["serviceName"] = req.serviceName;
   api.defaults.params["mgeSession"] = req.Cookie;
-  // api.defaults.headers.common["Cookie"] = req.Cookie;
-  //console.log(api.defaults);
+  //api.defaults.headers.common["Cookie"] = req.Cookie;
+  console.log(api.defaults);
   //res = await
   api
     .get("/api", req)
     .then((response) => {
       console.log(response.data);
-      //console.log("aki");
-      res = response.data;
+      console.log("aki");
+      //res = response.data;
     })
     .catch((error) => {
       // handle error
